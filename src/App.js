102459608fd6917d06dbafdc1b06fa9071d270cd
Seth-Carter <carter.seth.m@gmail.com>
2022-03-24 09:20:34 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>What's up, bitches!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
